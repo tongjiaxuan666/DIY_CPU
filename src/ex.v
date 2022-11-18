b4a2c9c24fcaf6a2d13b798f420bd22fde3a41fb
tjx <825830916@qq.com>
2022-11-18 11:41:16 +0800

    chapter 5-2 ,add sll ,sra...

--- src/ex.v
+++ src/ex.v
@@ -16,6 +16,7 @@ module ex (
 );
 //save the result of symbol compute
     reg[`RegBus] logicout;
+    reg[`RegBus] shiftres; // save shift result
 //stage one: compute with aluop_i
     always @(*) begin
         if(rst == `RstEnable) begin
@@ -25,12 +26,42 @@ module ex (
                 `EXE_OR_OP:begin
                     logicout <= reg1_i | reg2_i;
                 end
+                `EXE_AND_OP:begin
+                    logicout <= reg1_i & reg2_i;
+                end
+                `EXE_NOR_OP:begin
+                    logicout <= ~(reg1_i | reg2_i);
+                end
+                `EXE_XOR_OP:begin
+                    logicout <= reg1_i ^ reg2_i;
+                end
                 default: begin
                     logicout <= `ZeroWord;
                 end
             endcase
         end//if
     end//always
+// shift
+always @(*) begin
+    if(rst == `RstEnable) begin
+        shiftres <= `ZeroWord;
+    end else begin
+        case(aluop_i) 
+            `EXE_SLL_OP:begin
+                shiftres <= reg2_i << reg1_i[4:0];
+            end
+            `EXE_SRL_OP:begin
+                shiftres <= reg2_i >> reg1_i[4:0];
+            end
+            `EXE_SRA_OP:begin
+                shiftres <= ({32{reg2_i[31]}}<<(6'd32-{1'b0,reg1_i[4:0]})) | reg2_i >> reg1_i[4:0];
+            end
+            default: begin
+                shiftres <= `ZeroWord;
+            end
+        endcase
+        end//if
+end
 //stage two: compute instead of alusel_i
     always @(*) begin
         wd_o <= wd_i;
@@ -39,6 +70,9 @@ module ex (
             `EXE_RES_LOGIC: begin
                 wdata_o <= logicout;
             end 
+            `EXE_RES_SHIFT:begin
+                wdata_o <= shiftres;
+            end
             default: begin
                 wdata_o <= `ZeroWord;
             end
--- src/id.v
+++ src/id.v
@@ -37,7 +37,7 @@ wire[4:0] op4 = inst_i[20:16];
 // save need imediate num
 reg[`RegBus] imm;
 //judge vaildable
-reg instvaild;
+reg instvalid;
 //stage one : decode
 always @(*) begin
     if(rst ==`RstEnable) begin
@@ -45,7 +45,7 @@ always @(*) begin
         alusel_o <= `EXE_RES_NOP;
         wd_o   <= `NOPRegAddr;
         wreg_o <= `WriteDisable;
-        instvaild <= `InstVaild;
+        instvalid <= `InstValid;
         reg1_read_o <= 1'b0;
         reg2_addr_o <= 1'b0;
         reg1_addr_o <= `NOPRegAddr;
@@ -56,35 +56,181 @@ always @(*) begin
         alusel_o <= `EXE_RES_NOP;
         wd_o   <= inst_i[15:11];
         wreg_o <= `WriteDisable;
-        instvaild <= `InstInvaild;
+        instvalid <= `InstInvaild;
         reg1_read_o <= 1'b0;
         reg2_read_o <= 1'b0;
         reg1_addr_o <= inst_i[25:21];//default read from regfile pole one
         reg2_addr_o <= inst_i[20:16];//default read from regfile pole two
         imm <= `ZeroWord;       
     case (op)
+        `EXE_SPECIAL_INST: begin
+            case(op2)
+            5'b00000:   begin
+                case(op3) 
+                    `EXE_OR:    begin
+                        wreg_o <= `WriteEnable;		
+                        aluop_o <= `EXE_OR_OP;
+                        alusel_o <= `EXE_RES_LOGIC; 	
+                        reg1_read_o <= 1'b1;	
+                        reg2_read_o <= 1'b1;
+                        instvalid <= `InstValid;	
+                    end  
+                    `EXE_AND:	begin
+		    			wreg_o <= `WriteEnable;		
+                        aluop_o <= `EXE_AND_OP;
+		  				alusel_o <= `EXE_RES_LOGIC;	  
+                        reg1_read_o <= 1'b1;	
+                        reg2_read_o <= 1'b1;	
+		  				instvalid <= `InstValid;	
+					end 
+                    `EXE_XOR:	begin
+		    			wreg_o <= `WriteEnable;		
+                        aluop_o <= `EXE_XOR_OP;
+		  				alusel_o <= `EXE_RES_LOGIC;		
+                        reg1_read_o <= 1'b1;	
+                        reg2_read_o <= 1'b1;	
+		  				instvalid <= `InstValid;	
+					end  
+                    `EXE_NOR:	begin
+		    			wreg_o <= `WriteEnable;		
+                        aluop_o <= `EXE_NOR_OP;
+		  				alusel_o <= `EXE_RES_LOGIC;		
+                        reg1_read_o <= 1'b1;	
+                        reg2_read_o <= 1'b1;	
+		  				instvalid <= `InstValid;	
+					end 
+                    `EXE_SLLV: begin
+						wreg_o <= `WriteEnable;		
+                        aluop_o <= `EXE_SLL_OP;
+		  				alusel_o <= `EXE_RES_SHIFT;		
+                        reg1_read_o <= 1'b1;	
+                        reg2_read_o <= 1'b1;
+		  				instvalid <= `InstValid;	
+					end 
+                    `EXE_SRLV: begin
+					    wreg_o <= `WriteEnable;		
+                        aluop_o <= `EXE_SRL_OP;
+		  				alusel_o <= `EXE_RES_SHIFT;		
+                        reg1_read_o <= 1'b1;	
+                        reg2_read_o <= 1'b1;
+		  				instvalid <= `InstValid;	
+					end 
+                    `EXE_SRAV: begin
+						wreg_o <= `WriteEnable;		
+                        aluop_o <= `EXE_SRA_OP;
+		  				alusel_o <= `EXE_RES_SHIFT;		
+                        reg1_read_o <= 1'b1;	
+                        reg2_read_o <= 1'b1;
+		  				instvalid <= `InstValid;			
+		  			end
+                    `EXE_SYNC: begin
+						wreg_o <= `WriteDisable;		
+                        aluop_o <= `EXE_NOP_OP;
+		  				alusel_o <= `EXE_RES_NOP;		
+                        reg1_read_o <= 1'b0;	
+                        reg2_read_o <= 1'b1;
+		  				instvalid <= `InstValid;	
+					end
+                    default:begin
+					end
+                endcase//op3
+            end
+            default: begin
+                
+            end
+            endcase//op2
+        end    
         `EXE_ORI:   begin
-        //ori want to write in wanted write,so put wreg_o to writeable
-        wreg_o <= `WriteEnable;
-        //the subkind of compute is or
-        aluop_o <= `EXE_OR_OP;
-        //the kind of compute is  logic
-        alusel_o <= `EXE_RES_LOGIC;
-        //need Regfile one to read pole one
-        reg1_read_o <= 1'b1;
-        // not need Regfile two to read pole two
-        reg2_read_o <= 1'b0;
-        //execute immediate num
-        imm <= {16'h0, inst_i[15:0]};
-        //execute where you want to write in
-        wd_o <= inst_i[20:16];
-        // ori is Vaild;
-        instvaild <= `InstVaild;
-        end 
+            //ori want to write in wanted write,so put wreg_o to writeable
+            wreg_o <= `WriteEnable;
+            //the subkind of compute is or
+            aluop_o <= `EXE_OR_OP;
+            //the kind of compute is  logic
+            alusel_o <= `EXE_RES_LOGIC;
+            //need Regfile one to read pole one
+            reg1_read_o <= 1'b1;
+            // not need Regfile two to read pole two
+            reg2_read_o <= 1'b0;
+            //execute immediate num
+            imm <= {16'h0, inst_i[15:0]};
+            //execute where you want to write in
+            wd_o <= inst_i[20:16];
+            // ori is Vaild;
+            instvalid <= `InstValid;
+            end 
+        `EXE_ANDI:	begin
+		  	wreg_o <= `WriteEnable;		
+            aluop_o <= `EXE_AND_OP;
+		  	alusel_o <= `EXE_RES_LOGIC;	
+            reg1_read_o <= 1'b1;	
+            reg2_read_o <= 1'b0;	  	
+			imm <= {16'h0, inst_i[15:0]};		
+            wd_o <= inst_i[20:16];		  	
+			instvalid <= `InstValid;	
+			end	 
+
+	    `EXE_XORI:	begin
+		  	wreg_o <= `WriteEnable;		
+            aluop_o <= `EXE_XOR_OP;
+		  	alusel_o <= `EXE_RES_LOGIC;	
+            reg1_read_o <= 1'b1;	
+            reg2_read_o <= 1'b0;	  	
+			imm <= {16'h0, inst_i[15:0]};		
+            wd_o <= inst_i[20:16];		  	
+			instvalid <= `InstValid;	
+			end	
+        `EXE_LUI:	begin
+		  	wreg_o <= `WriteEnable;		
+            aluop_o <= `EXE_OR_OP;
+		  	alusel_o <= `EXE_RES_LOGIC; 
+            reg1_read_o <= 1'b1;	
+            reg2_read_o <= 1'b0;	  	
+			imm <= {inst_i[15:0], 16'h0};		
+            wd_o <= inst_i[20:16];		  	
+			instvalid <= `InstValid;	
+			end		
+		`EXE_PREF:	begin
+		  	wreg_o <= `WriteDisable;		
+            aluop_o <= `EXE_NOP_OP;
+		  	alusel_o <= `EXE_RES_NOP; 
+            reg1_read_o <= 1'b0;	
+            reg2_read_o <= 1'b0;	  	  	
+			instvalid <= `InstValid;	
+			end 
         default: begin
             
         end
     endcase// case op
+    if (inst_i[31:21] == 11'b00000000000) begin
+		if (op3 == `EXE_SLL) begin
+		  	wreg_o <= `WriteEnable;		
+            aluop_o <= `EXE_SLL_OP;
+		  	alusel_o <= `EXE_RES_SHIFT; 
+            reg1_read_o <= 1'b0;	
+            reg2_read_o <= 1'b1;	  	
+			imm[4:0] <= inst_i[10:6];		
+            wd_o <= inst_i[15:11];
+			instvalid <= `InstValid;	
+		end else if ( op3 == `EXE_SRL ) begin
+		  	wreg_o <= `WriteEnable;		
+            aluop_o <= `EXE_SRL_OP;
+		  	alusel_o <= `EXE_RES_SHIFT; 
+            reg1_read_o <= 1'b0;	
+            reg2_read_o <= 1'b1;	  	
+			imm[4:0] <= inst_i[10:6];		
+            wd_o <= inst_i[15:11];
+			instvalid <= `InstValid;	
+		end else if ( op3 == `EXE_SRA ) begin
+		  	wreg_o <= `WriteEnable;		
+            aluop_o <= `EXE_SRA_OP;
+		  	alusel_o <= `EXE_RES_SHIFT; 
+            reg1_read_o <= 1'b0;	
+            reg2_read_o <= 1'b1;	  	
+			imm[4:0] <= inst_i[10:6];		
+            wd_o <= inst_i[15:11];
+			instvalid <= `InstValid;	
+		end
+	end	
     end //if
 end // always
 //stage two : confirm source data one
@@ -114,7 +260,7 @@ always @(*) begin
     end else if((reg2_read_o == 1'b1) && (mem_wreg_i == 1'b1) && (mem_wd_i == reg2_addr_o)) begin
         reg2_o <= mem_wdata_i;
     end else if(reg2_read_o == 1'b1) begin
-        reg2_o <= reg1_data_i; // Regfile Readone as input
+        reg2_o <= reg2_data_i; // Regfile Readone as input
     end else if (reg2_read_o == 1'b0) begin
         reg2_o <= imm; //imediate num
     end else begin
